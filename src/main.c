8cb840869384be3f991155f75c73219bc3e88766
toxypiks <HannaHinkelbein89@gmx.de>
2025-06-21 11:49:56 +0200

    Include SDL3 into project and create window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <SDL3/SDL.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 
 int main(void)
 {
+    SDL_Window *window;                    // Declare a pointer
+    bool done = false;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != true) {
+        fprintf(stderr, "Could not initialize sdl3: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    window = SDL_CreateWindow("Procedural", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    while (!done) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                done = true;
+            }
+        }
+
+        // Do game logic, present a frame, etc.
+    }
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
     return 0;
 }
