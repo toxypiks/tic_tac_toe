a59cebaaffd54e6a408b003ef0cd6826b760dc2b
toxypiks <HannaHinkelbein89@gmx.de>
2025-06-26 16:23:06 +0200

    Cast mouse position to int

--- src/main.c
+++ src/main.c
@@ -20,14 +20,16 @@ int main(void)
     };
 
     while(!WindowShouldClose()) {
-        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-            Vector2 mouse_position = GetMousePosition();
-            click_on_cell(&game,
-                          mouse_position.y / CELL_HEIGHT,
-                          mouse_position.x / CELL_WIDTH);
-                      }
+
         BeginDrawing();
         render_game(&game);
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            Vector2 mouse_position = GetMousePosition();
+            printf("x : %d, y: %d\n", (int)(mouse_position.x / CELL_WIDTH), (int)(mouse_position.y / CELL_HEIGHT));
+            click_on_cell(&game,
+                          (int)(mouse_position.y / CELL_HEIGHT),
+                          (int)(mouse_position.x / CELL_WIDTH));
+                      }
         ClearBackground(GetColor(0x181818AA));
         EndDrawing();
 
